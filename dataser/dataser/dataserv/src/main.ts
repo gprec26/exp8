59621ef779a89e6dec893c6c1f1337e51d91c8fe
gprec26 <229x1a0534@gprec.ac.in>
2025-10-29 07:04:40 +0530

    Delete dataser/dataser/dataserv directory

--- dataser/dataser/dataserv/src/main.ts
+++
@@ -1,7 +0,0 @@
-import 'zone.js';  // In TERMINAL npm install zone.ts
- import { bootstrapApplication } from '@angular/platform-browser';
- import { App } from './app/app';
- import { provideHttpClient } from '@angular/common/http';
- bootstrapApplication(App, {
-  providers: [provideHttpClient()]
- });
